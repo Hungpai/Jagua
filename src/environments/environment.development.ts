3df62986e8d16bfcf3e1e861156765404da273da
Hung <t_hung@hotmail.de>
2025-02-01 23:08:37 +0100

    New api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // apiBaseUrl: 'http://localhost:8080'
-    apiBaseUrl: 'http://ec2-54-93-99-163.eu-central-1.compute.amazonaws.com:8080'
+    apiBaseUrl: 'https://jagua-api.hungpy.de:444'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://ec2-54-93-99-163.eu-central-1.compute.amazonaws.com:8080'
+    apiBaseUrl: 'https://jagua-api.hungpy.de:444'
 };
